b82173c88b747e8b1ed75ac8545658fbfc50dc6f
danielpascari <vajno.1997@mail.ru>
2019-04-21 12:35:04 +0300

    transfer from z_elements branch to master

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/Vide-master/dist/jquery.vide.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
